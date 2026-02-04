e2ca2bb321da7e11c808e0ee77a03c24b76c0bc6
William Amponsah <Francescpacopablo@gmail.com>
2026-02-04 17:31:20 +0000

    https://res.cloudinary.com/ddvlexmvj/image/upload/v1770226242/boku_horiz

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="https://res.cloudinary.com/ddvlexmvj/image/upload/v1770226118/boku_horizontal_matched_font_bdcakk.svg"
+      src="https://res.cloudinary.com/ddvlexmvj/image/upload/v1770226242/boku_horizontal_matched_font-removebg-preview_v2ax4m.png"
       alt="Boku Partners Logo"
       width={150}
       height={40}

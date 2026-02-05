112568e3565056841e89c69ccf64af83cb557003
William Amponsah <Francescpacopablo@gmail.com>
2026-02-05 19:33:13 +0000

    Reduce the height of the logo image much more to match the height if the

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,8 +5,8 @@ export function Logo() {
     <Image
       src="https://res.cloudinary.com/ddvlexmvj/image/upload/v1770227035/boku_horizontal_matched_font-Photoroom_1_p8gexh.png"
       alt="Boku Partners Logo"
-      width={150}
-      height={40}
+      width={90}
+      height={24}
       priority
     />
   );

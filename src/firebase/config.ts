c8b2ca2b2d245a1e1807e17f568c0e87eac9394b
William Amponsah <Francescpacopablo@gmail.com>
2026-04-17 20:50:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-718123490-6d6ae",
+  "appId": "1:593788099641:web:ac947eafe4cd339b0f41e8",
+  "apiKey": "AIzaSyA8UQlSDb8wl3W9Q20FinRg8E8H2FMvZcs",
+  "authDomain": "studio-718123490-6d6ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "593788099641"
+};
